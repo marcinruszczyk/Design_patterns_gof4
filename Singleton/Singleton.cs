a830f1a8d59c4a6e237bc438fb9aaf86c218b6c6
marcinruszczyk <ruszmar@gmail.com>
2024-11-24 17:09:33 +0100

    Singleton

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_pattern
+{
+    public class Singleton
+    {
+        static Singleton instance;
+
+        public Singleton()
+        {
+            
+        }
+
+        public static Singleton Instance()
+        {
+            if (instance == null) 
+            {
+                instance = new Singleton(); 
+            }
+            return instance;
+        }
+    }
+}
