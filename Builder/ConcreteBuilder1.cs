859dc5f4344bd201e8fa39630951c7fa6d71a7c3
marcinruszczyk <ruszmar@gmail.com>
2024-11-24 12:33:02 +0100

    Builder desing pattern

---
+++ Builder/ConcreteBuilder1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_pattern
+{
+    class ConcreteBuilder1 : Builder
+    {
+        private Product _product = new Product();
+
+        public override void BuildPartA()
+        {
+            _product.Add("PartA");
+        }
+
+        public override void BuildPartB()
+        {
+            _product.Add("PartB");
+        }
+
+        public override Product GetResult()
+        {
+            return _product;
+        }
+
+    }
+}
