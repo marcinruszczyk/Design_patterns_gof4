2393d18dc81736ead81957df52488d0c6af48931
marcinruszczyk <ruszmar@gmail.com>
2024-11-24 17:32:58 +0100

    Adapter

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_pattern
+{
+    public class Adapter : Target
+    {
+        private Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+}
