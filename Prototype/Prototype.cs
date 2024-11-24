7b59e1cbf6094b541321da285af72d148214f05d
marcinruszczyk <ruszmar@gmail.com>
2024-11-24 16:49:38 +0100

    Prototype pattern

---
+++ Prototype/Prototype.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_pattern
+{
+    public abstract class Prototype
+    {
+        string id;
+
+        public Prototype(string id)
+        {
+            this.id = id;
+        }
+
+        public string Id
+        {
+            get { return id; }
+        }
+
+        public abstract Prototype Clone();
+
+    }
+}
