cabf56363d4cdd2696cda5977f943e5a29d5cc45
marcinruszczyk <ruszmar@gmail.com>
2024-11-24 18:47:17 +0100

    Decorator

---
+++ Decorator/Decorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_pattern
+{
+    public abstract class Decorator : Component
+    {
+        protected Component component;
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+}
--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Decorator_pattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Create ConcreteComponent and two Decorators
+
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+            //Link decorators
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+
+            d2.Operation();
+
+            Console.ReadKey();
+        }
+    }
+}
