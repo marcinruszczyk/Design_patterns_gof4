313f80c5a6adbbb3ff20cbebf95ef848d93b058f
marcinruszczyk <ruszmar@gmail.com>
2024-12-02 20:54:49 +0100

    command design pattern

---
+++ Command/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_pattern
+
+{
+    public abstract class Command
+    {
+        protected Receiver receiver;
+
+        public Command(Receiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+}
