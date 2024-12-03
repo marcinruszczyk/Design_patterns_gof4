f3ce7703c701f42cfa4169514ebf66150b580867
marcinruszczyk <ruszmar@gmail.com>
2024-12-03 22:15:34 +0100

    memento design pattern

--- Memento/Program.cs
+++ Memento/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Memento_pattern
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Originator o = new Originator();
+            o.State = "On";
+
+            //Store internal state
+
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+
+            //continue changing originator
+
+            o.State = "off";
+
+            // restore saved state
+
+            o.SetMemento(c.Memento);
+
+            //wait for user
+            Console.ReadKey();
+        }
+    }
+}
