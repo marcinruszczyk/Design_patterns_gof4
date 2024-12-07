28056570ff4068853d82b4f9ece3d148ea74f439
marcinruszczyk <ruszmar@gmail.com>
2024-12-07 14:19:11 +0100

    state and strategy

---
+++ Strategy/ConcreteStrategyA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_pattern
+{
+    public class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("Called ConcreteStrategyA.AlgorithmInterface()");
+        }
+    }
+}
---
+++ Strategy/ConcreteStrategyC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_pattern
+{
+    public class ConcreteStrategyC : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine("Called ConcreteStrategyC.AlgorithmInterface()");
+        }
+    }
+}
